bf01dcd081aca5ea3416bca9b71f125025a39f30
Samit Koyom <samitkoyom@gmail.com>
2021-03-13 01:53:05 +0700

    update gitignoer and www

---
+++ www/booking/index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+
+echo "Hello PHP 7.4<br>";
+
+$var = 200.75;
+$arr = array('coffee','tea','milk','soda','water');
+
+for($i=1;$i<=20;$i++){
+    echo $i."<br>";
+}
+
+foreach($arr as $val){
+    echo $val."<br>";
+}
+
+// New feature in PHP 7
+echo 1 <=> 1; // 0
+echo 1 <=> 2; // -1
+echo 2 <=> 1; // 1
